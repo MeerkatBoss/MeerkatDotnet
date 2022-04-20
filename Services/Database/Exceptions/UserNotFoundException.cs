ea9717581933f956018e227d62906271b2a4ed79
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-04-20 18:34:35 +0300

    fix(Services): exceptions were not defined

---
+++ Services/Database/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,13 @@
+using System.Runtime.Serialization;
+
+namespace MeerkatDotnet.Services.Database.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException() { }
+    public UserNotFoundException(string message) : base(message) { }
+    public UserNotFoundException(string message, Exception inner) : base(message, inner) { }
+    public UserNotFoundException(
+        SerializationInfo info, StreamingContext context
+        ) : base(info, context) { }
+}
